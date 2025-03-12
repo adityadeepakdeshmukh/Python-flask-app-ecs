dad32e5837083483962ded7058c25604ac7ac18e
Aditya Deshmukh <140731357+adityadeepakdeshmukh@users.noreply.github.com>
2025-03-12 16:28:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello_geek():
     return '<h1>Hello from Flask & Docker</h1>'
 @app.route('/hi')
 def hell():
-    return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
+    return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
